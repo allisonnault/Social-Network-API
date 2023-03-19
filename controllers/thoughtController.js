431afd0b93605fa0384abba991e15bbf4d5cde36
allisonnault <allisonrnault@gmail.com>
2023-03-19 16:24:08 -0500

    thought by id working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,44 +1,59 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    getThoughts(req,res) {
+    // get all thoughts
+    getThoughts(req, res) {
         Thought.find()
             .then((thought) => res.json(thought))
             .catch((err) => res.status(500).json(err));
     },
 
+    //get thought by _id
     getSingleThought(req, res) {
-        Thought.findOne({ _id: req.paramas.thoughtId })
-            .then((thought) => 
-            !thought
-            ? res.status(404).json({ message: "No thought found with that ID" })
-            : res.json(thought)
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No thought found with that ID" })
+                    : res.json(thought)
             )
             .catch((err) => res.status(500).json(err));
     },
 
-createThought(req, res) {
-    Thought.create(req.body)
-    .then((thought) => {
-        return User.findOneAndUpdate(
-            { username: req.body.username },
-            { $addToSet: { thoughts: thought._id } },
-            { new: true }
-        );
-    })
-    .then((user) => 
-    !user
-    ? res.status(404).json({ message: "Thought created, but found no user with that ID" })
-    : res.json('Create the thought')
-    )
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-},
-// updateThought,
-// deleteThought,
-// addReaction,
-// deleteReaction,
+    //create a thought
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { username: req.body.username },
+                    { $addToSet: { thoughts: thought._id } },
+                    { new: true }
+                );
+            })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "Thought created, but found no user with that ID" })
+                    : res.json('thought created')
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    
+    // updateThought(req, res) {
+
+    // },
+
+    // deleteThought(req, res) {
+
+    // },
+
+    // addReaction(req, res) {
+
+    // },
+
+    // deleteReaction (req, res) {
+
+    // },
 
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,7 +12,7 @@ const {
 
 router.route('/').get(getThoughts).post(createThought);
 
-// router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought)//.put(updateThought).delete(deleteThought);
 
 // router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
 
